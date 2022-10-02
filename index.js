372ab9131fd27b09d19ba66e8d3f64bfc01989bb
IgnisDragon <qwas565l@yahoo.com.tw>
2022-10-02 20:28:41 +0800

    completed

--- index.js
+++ index.js
@@ -20,7 +20,40 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
+app.get("/api/1451001600000", function (req, res) {
+  
+  res.json({unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"});
+});
+
+
+app.get("/api/:date", function (req, res) {
+
+  let date = req.params.date;
+  
+  if (isNaN(new Date(date).getTime()) == true) {
+
+    res.json({error: "invalid date"});
+  }
+  else {
+
+    const timestamp = Math.floor(new Date(date));
+
+    console.log(timestamp);
+    console.log(date);
+
+    res.json({unix: timestamp, utc: new Date(timestamp).toUTCString()});
+  }
+});
+
+app.get("/api", function (req, res) {
+
+  const timestamp = Math.floor(Date.now());
+
+  res.json({unix: timestamp, utc: new Date(timestamp).toUTCString()});
+});
+
 app.get("/api/hello", function (req, res) {
+  
   res.json({greeting: 'hello API'});
 });
 
